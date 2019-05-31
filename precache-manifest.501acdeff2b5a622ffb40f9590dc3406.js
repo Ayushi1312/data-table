0995adc86405389fa3b50429da522500e1dc867f
Ayushi <ayushichhabra.itm@gmail.com>
2019-05-31 16:04:15 +0200

    init

---
+++ precache-manifest.501acdeff2b5a622ffb40f9590dc3406.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cd61f6ce050195bfed914dd114e90957",
+    "url": "/index.html"
+  },
+  {
+    "revision": "239f2b02421f1e77bef3",
+    "url": "/static/css/main.8c83990f.chunk.css"
+  },
+  {
+    "revision": "c5b0ecde2d034ee50b5b",
+    "url": "/static/js/2.02e28368.chunk.js"
+  },
+  {
+    "revision": "239f2b02421f1e77bef3",
+    "url": "/static/js/main.54843c4b.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
